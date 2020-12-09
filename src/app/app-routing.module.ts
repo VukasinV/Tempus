a9f079ac3ef947492fb423f3d5552d6b3049a3a0
VukasinV <vranic.vukasin@gmail.com>
2020-12-09 23:37:20 +0100

    added welcome and home page, login and signup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  {
+    path: 'home', 
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
